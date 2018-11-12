b7c4b7e5b77f950f481eb435990d908eda8c4929
siggismara <runningman69@gmail.com>
2018-11-12 10:23:08 +0100

    Combining all in one app

--- R/fiaShinyServer.R
+++ R/fiaShinyServer.R
@@ -13,17 +13,161 @@ library(shinyjs)
 fiaShinyServer <- function(self) {
 return(function(input, output, session) {
 
+  output$note <- renderText({ "* Note: should be present in the Result directory" })
+
   startupFilter <- reactive({
+    genText <- paste("This Shiny App is intended to be run",
+                     "as a part of a larger workflow in which some objects would",
+                     "already be defined in this environment prior to `runApp` being",
+                     "executed. Try evaluating the code in `parent.R` which",
+                     "wraps this Shiny app in a larger workflow.")
+    if (!"workdirPath" %in% names(self$settings)) {
+      stop(paste("'workdirPath' var doesn't exist.",genText))
+    }
+    #populate the settings
+    updateDirectoryInput(session, 'mzmlDirectory', value = self$settings$workdirPath)
+
+    if(file.exists(file.path(self$settings$workdirRDataPath,'settings.RData'))) {
+      load(file.path(self$settings$workdirRDataPath,'settings.RData'))
+      self$settings <- settings
+    } else {
+      showNotification("No settings file detected. Using defaults.", type='warning')
+    }
+    settings <- self$settings
+    updateDirectoryInput(session, 'wiffDirectory', value = as.character(self$settings$wiffPath))
+    updateDirectoryInput(session, 'protwizDirectory', value = as.character(self$settings$protwizPath))
+    updateCheckboxInput(session,'convertNewWiffs', value =as.logical(self$settings$convertWiffs))
+    #updateCheckboxInput(session,'useParallel', value =as.logical(self$settings$useParallel))
+    updateTextInput(session, 'fiaFile', value = as.character(self$settings$fiaFile))
+    updateTextInput(session, 'reloadData', value = as.logical(self$settings$reloadData))
+    updateTextInput(session, 'forceRecalc', value = as.logical(self$settings$forceRecalc))
+    updateTextAreaInput(session, 'fiaFeatures', value = as.character(self$settings$fiaFeatures))
+    return('')
+  })
+
+  startupFilter2 <- reactive({
     updateSelectInput(session, 'metaboliteID', choices = myAnalytes())
     updateSelectInput(session, 'filterYear', choices = c('select a year',allYears()))
     updateSelectInput(session, 'batchID', choices = myBatches(), selected = input$batchID)
     return('')
   })
 
-  output$filter2 <- renderText({
+  output$filter <- renderText({
     startupFilter()
   })
 
+  output$filter2 <- renderText({
+    startupFilter2()
+  })
+
+  observeEvent(
+    ignoreNULL = TRUE,
+    eventExpr = {
+      input$mzmlDirectory
+    },
+    handlerExpr = {
+      # condition prevents handler execution on initial app launch
+      if (input$mzmlDirectory > 0) {
+        # launch the directory selection dialog with initial path read from the widget
+        path = choose.dir(default = readDirectoryInput(session, 'mzmlDirectory'),
+                          'Select the result directory...')
+
+        # update the widget value
+        updateDirectoryInput(session, 'mzmlDirectory', value = path)
+      }
+    }
+  )
+
+  observeEvent(
+    ignoreNULL = TRUE,
+    eventExpr = {
+      input$wiffDirectory
+    },
+    handlerExpr = {
+      # condition prevents handler execution on initial app launch
+      if (input$wiffDirectory > 0) {
+        # launch the directory selection dialog with initial path read from the widget
+        path = choose.dir(default = readDirectoryInput(session, 'wiffDirectory'),
+                          'Select the raw data directory...')
+
+        # update the widget value
+        updateDirectoryInput(session, 'wiffDirectory', value = path)
+      }
+    }
+  )
+
+  observeEvent(
+    ignoreNULL = TRUE,
+    eventExpr = {
+      input$protwizDirectory
+    },
+    handlerExpr = {
+      # condition prevents handler execution on initial app launch
+      if (input$protwizDirectory > 0) {
+        # launch the directory selection dialog with initial path read from the widget
+        path = choose.dir(default = readDirectoryInput(session, 'protwizDirectory'),
+                          'Select the Proteowizard directory...')
+
+        # update the widget value
+        updateDirectoryInput(session, 'protwizDirectory', value = path)
+      }
+    }
+  )
+
+  observeEvent(input$fiaFile, {
+    if(!file.exists(file.path(self$settings$workdirPath,input$fiaFile))) {
+      shinyjs::runjs(paste0("document.getElementById('fiaFile').style.border =
+                     'solid red'"))
+    } else {
+      shinyjs::runjs(paste0("document.getElementById('fiaFile').style.border =
+                     ''"))
+    }
+  })
+
+  observeEvent(input$saveSettings, {
+    settings <- self$settings
+    self$settings$fiaFile <- input$fiaFile
+    #self$settings$fiaIstdFile <- input$fiaIstdFile
+    self$settings$fiaFeatures <- unlist(str_split(input$fiaFeatures,','))
+    self$settings$workdirPath <- readDirectoryInput(session, 'mzmlDirectory')
+    self$settings$wiffPath <- readDirectoryInput(session, 'wiffDirectory')
+    self$settings$convertWiffs <- input$convertNewWiffs
+    self$settings$reloadData <- input$reloadData
+    self$settings$forceRecalc <- input$forceRecalc
+
+    save(settings, file=file.path(self$settings$workdirRDataPath,'settings.RData'))
+    showNotification("Settings saved", type='message')
+  })
+
+  observe({
+    if (input$Continue > 0) {
+      self$settings$fiaFile <- input$fiaFile
+      #self$settings$fiaIstdFile <- input$fiaIstdFile
+      self$settings$fiaFeatures <- unlist(str_split(input$fiaFeatures,','))
+      self$settings$workdirPath <- readDirectoryInput(session, 'mzmlDirectory')
+      self$settings$wiffPath <- readDirectoryInput(session, 'wiffDirectory')
+      self$settings$convertWiffs <- input$convertNewWiffs
+      self$settings$reloadData <- input$reloadData
+      self$settings$forceRecalc <- input$forceRecalc
+
+      # Create a Progress object
+      progress <- shiny::Progress$new()
+      progress$set(message = "Preparing data", value = 0)
+      # Close the progress when this reactive exits (even if there's an error)
+      on.exit(progress$close())
+
+      updateProgress <- function(value = NULL, detail = NULL) {
+        if (is.null(value)) {
+          value <- progress$getValue()
+          value <- value + (progress$getMax() - value) / 5
+        }
+        progress$set(value = value, detail = detail)
+      }
+      self$prepareForFIA(updateProgress)
+      #switch to next panel?
+      #stopApp()  # stop shiny
+    }
+  })
   observeEvent(input$filterYear, {
     if(length(self$myUIdata$allDates) > 0){
       mymonths <- self$myUIdata$allDates[year(self$myUIdata$allDates) == input$filterYear]
--- R/fiaShinyUI.R
+++ R/fiaShinyUI.R
@@ -11,6 +11,50 @@ library(shinyjs)
 fiaShinyUI <-function(self) {
   shinyUI(
     navbarPage("FIA SS Explorer",
+          tabPanel("Settings",
+                   sidebarLayout(
+                     sidebarPanel(
+                       #actionLink("recalcAll", "Recalc All"),
+                       shinyjs::useShinyjs(),
+                       directoryInput('mzmlDirectory',
+                                      label = 'Results directory'
+                       ),
+                       directoryInput('wiffDirectory',
+                                      label = 'Raw Data directory'
+                       ),
+                       directoryInput('protwizDirectory',
+                                      label = 'Proteowizard directory'
+                       ),
+                       checkboxInput('convertNewWiffs', 'Look for and convert new raw data files', value = TRUE),
+                       checkboxInput('reloadData', 'Reload results (Will only calculate new results if found)', value = FALSE),
+                       checkboxInput('forceRecalc', 'Recalculate ALL results (Warning! This takes a long time)', value = FALSE),
+                       # conditionalPanel(
+                       #   condition = "input.useParallel == true",
+                       #   textInput('multicores','How many cores to use?',value='3')
+                       # ),
+                       textInput('fiaFile', 'MRM transitions file name **'),
+                       tags$div(class="h5", checked=NA,
+                                tags$p("** Note: should be present in the Results directory")
+                       ),
+                       textAreaInput('fiaFeatures', 'Features for SS check'),
+                       actionButton("saveSettings",'Save'),
+
+                       tags$button(
+                         id = 'Continue',
+                         type = "button",
+                         class = "btn action-button",
+                         onclick = "setTimeout(function(){window.close();},500);",  # close browser
+                         "Continue"
+                       ),
+                       width = 10
+                     ),
+
+                     # Show a plot of the generated distribution
+                     mainPanel(
+                       textOutput("filter")
+                     )
+                   )
+          ),
            tabPanel("Time Trends",
                     # Sidebar for the time trend explorer
                     sidebarLayout(
